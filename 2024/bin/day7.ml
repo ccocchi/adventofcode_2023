8dd623e32a18c1c9af43e224023ccccbe4c9e585
ccocchi <cocchi.c@gmail.com>
2024-12-07 15:11:47 +0100

    Day 7b

--- 2024/bin/day7.ml
+++ 2024/bin/day7.ml
@@ -1,11 +1,43 @@
 open Core
 
-let has_loop (list: int list) =
-  let rec inner l1 l2 = match (l1, l2) with
-  | (x :: xs, _ :: y :: ys) -> Int.equal x y || inner xs ys
-  | _ -> false
+let parse_input =
+  In_channel.read_lines "/Users/chris/code/adventofcode_2023/2024/data/day7.txt"
+  |> List.map ~f:(fun l -> match String.split ~on:':' l with
+    | res :: op :: _ -> (res, String.split ~on:' ' (String.lstrip op))
+    | _ -> raise(Invalid_argument("a"))
+  )
+  |> List.map ~f:(fun (x, l) -> (int_of_string x, List.map ~f:int_of_string l))
+
+let isSolvable expected inputs with_concat =
+  let rec inner n l =
+    if (n < 0) then false
+    else
+      match l with
+      | [] -> true
+      | e :: [] -> Int.equal n e
+      | x :: xs ->
+        (Int.equal Int.zero (n mod x) && inner (n / x) xs)
+        || inner (n - x) xs
+        || with_concat && (
+          let str = string_of_int n and s = string_of_int x in
+          try
+            String.is_suffix str ~suffix:s && inner (int_of_string (String.drop_suffix str (String.length s))) xs
+          with
+          | Failure(_) -> true
+        )
 in
-  inner list list
+  inner expected (List.rev inputs)
+
+let part1 (equations: (int * int list) list) =
+  List.filter ~f:(fun (expected, inputs) -> isSolvable expected inputs false) equations
+  |> List.fold ~init:Int.zero ~f:(fun acc (x, _) -> acc + x)
+
+let part2 (equations: (int * int list) list) =
+  List.filter ~f:(fun (expected, inputs) -> isSolvable expected inputs true) equations
+  |> List.fold ~init:Int.zero ~f:(fun acc (x, _) -> acc + x)
 
 let () =
-  printf "%b\n" (has_loop [2;0;6;3;1;4;2;5;3;1;4;2])
+  let input = parse_input
+in
+  printf "part1=%d\n" (part1 input);
+  printf "part2=%d\n" (part2 input)
