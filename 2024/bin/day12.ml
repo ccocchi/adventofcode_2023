b668fe35f3d5890ade5ae37d14a71b199a645c2e
ccocchi <cocchi.c@gmail.com>
2024-12-12 16:29:26 +0100

    Part 2

--- 2024/bin/day12.ml
+++ 2024/bin/day12.ml
@@ -20,27 +20,27 @@ let parse_input =
   );
   points
 
-let neighbors (i, j) = [(i + 1, j); (i - 1, j); (i, j + 1); (i, j - 1)]
-
 let same table p c = match Hashtbl.find table p with
 | Some(v) -> Char.equal v c
 | None -> false
 
+let neighbors (i, j) = [(i + 1, j); (i - 1, j); (i, j + 1); (i, j - 1)]
+
 let make_regions table =
   let points = Set.of_hashtbl_keys (module Point) table in
   let expand_region set initial =
-    let rec inner (acc_p, acc_a) s = function
+    let rec inner acc s = function
     | p :: tl ->
       let c = Hashtbl.find_exn table p
       and ns = Set.remove s p in
       let positions = List.filter (neighbors p) ~f:(fun p -> same table p c) in
       inner
-        (acc_p + 4 - List.length positions, acc_a + 1)
+        (p :: acc)
         (List.fold positions ~init:ns ~f:Set.remove)
         (List.filter positions ~f:(Set.mem ns) @ tl)
-    | [] -> ((acc_p, acc_a), s)
+    | [] -> (acc, s)
     in
-    inner (0, 0) set [initial]
+    inner [] set [initial]
   in
   let rec inner acc s = match Set.min_elt s with
   | None -> acc
@@ -48,11 +48,41 @@ let make_regions table =
 in
   inner [] points
 
-let part1 = List.fold ~init:0 ~f:(fun acc (p, a) -> acc + (p * a))
+let part1 table regions =
+  let perimeter l = List.fold l ~init:0 ~f:(fun acc p ->
+    let c = Hashtbl.find_exn table p in
+    acc + List.count (neighbors p) ~f:(fun n -> same table n c |> not)
+  )
+  in
+  List.fold regions ~init:0 ~f:(fun acc l -> acc + (perimeter l) * List.length l)
+
+let part2 table regions =
+  let count_corners l =
+    let corners (i, j) = [
+      ((i - 1, j), (i, j + 1), (i - 1, j + 1));
+      ((i, j + 1), (i + 1, j), (i + 1, j + 1));
+      ((i + 1, j), (i, j - 1), (i + 1, j - 1));
+      ((i, j - 1), (i - 1, j), (i - 1, j - 1))
+    ]
+    in
+    List.fold l ~init:0 ~f:(fun acc pos ->
+      let c = Hashtbl.find_exn table pos in
+      acc + List.fold (corners pos) ~init:0 ~f:(fun acc (p1, p2, p3) ->
+        if ((same table p1 c) && (same table p2 c) && not (same table p3 c)) then (* inner corner *)
+          acc + 1
+        else if ((not (same table p1 c)) && (not (same table p2 c))) then (* outer corner *)
+          acc + 1
+        else
+          acc
+      )
+    )
+  in
+  List.fold regions ~init:0 ~f:(fun acc l -> acc + (count_corners l) * List.length l)
 
 let () =
   let table = parse_input in
   let regions = make_regions table in
-  printf "part1=%d\n" (part1 regions)
+  printf "part1=%d\n" (part1 table regions);
+  printf "part2=%d\n" (part2 table regions)
 
 
